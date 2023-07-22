ecbf78aa7af3112afabbc5a0ba6f0106547cf514
lelikone777 <2257855@gmail.com>
2023-07-22 16:03:19 +0400

    last

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -21,7 +21,7 @@ function Navbar() {
                     <ul className="nav-list">
 
                         <li className="nav-list__item">
-                            <NavLink to='/' className={({isActive}) => {
+                            <NavLink to='../' className={({isActive}) => {
                                 return isActive ? activeLink : normalLink;
                             }}>
                                 Home
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,15 +7,16 @@ const HomePage = () => {
 
             <main className="section" >
                 <div className="container">
-                    <h1 className="title-1">Skills</h1>
+                    <h1 className="title-1">My stack:</h1>
+                    {/*<h1 className="title-1">Skills</h1>*/}
                     <ul className="content-list">
-                        <li className="content-list__item"><h2 className="title-2">Frontend</h2>
-                            <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI,
-                                Yarn, TailwindCSS, StyledComponents</p>
+                        <li className="content-list__item">
+                            {/*<h2 className="title-2">Frontend</h2>*/}
+                            <p>JavaScript, React, NextJS13, Tailwind CSS</p>
                         </li>
                         <li className="content-list__item">
-                            <h2 className="title-2">Backend</h2>
-                            <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
+                            {/*<h2 className="title-2">Backend</h2>*/}
+                            {/*<p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>*/}
                         </li>
                     </ul>
                 </div>
