9565ebd264285cc84cf4f32f9ec64edb3d326475
lelikone777 <2257855@gmail.com>
2023-09-24 22:27:06 +0400

    README.md

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,7 +12,14 @@ const HomePage = () => {
                     <ul className="content-list">
                         <li className="content-list__item">
                             {/*<h2 className="title-2">Frontend</h2>*/}
-                            <p>JavaScript, React, NextJS13, Tailwind CSS</p>
+                            <ul>
+                                <li>JavaScript ES6+</li>
+                                <li>React JS, React Router</li>
+                                <li>Redux, Redux Toolkit, React Query</li>
+                                <li>NextJS 13+</li>
+                                <li>Webpack, Vite</li>
+                                <li>SCSS/SASS, Tailwind, Bootstrap 5</li>
+                            </ul>
                         </li>
                         <li className="content-list__item">
                             {/*<h2 className="title-2">Backend</h2>*/}
